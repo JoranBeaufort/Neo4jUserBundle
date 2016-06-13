75898ae97b8cead7527dced368490f5d936715f3
JoranBeaufort <mbaer@geonet.ch>
2016-06-13 19:18:30 +0200

    atom

--- src/DependencyInjection/Neo4jUserExtension.php
+++ src/DependencyInjection/Neo4jUserExtension.php
@@ -27,6 +27,6 @@ class Neo4jUserExtension extends Extension
         
         // Once the services definition are read, get your service and add a method call to setConfig()
         $serviceDefintion = $container->getDefinition( 'neo4j.graph_manager' );
-        $serviceDefintion->addMethodCall( 'setConfig', array( $config[ 'neo4j_user' ] ) );
+        $serviceDefintion->addMethodCall( 'setConfig', $config );
     }
 }
